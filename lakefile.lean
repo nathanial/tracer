6cccbd9d13e2bca14e22fd73e6e63d05314abb0a
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-01-08 18:17:17 -0800

    Update crucible to v0.0.7 and track lake-manifest.json

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package tracer where
   version := v!"0.1.0"
 
-require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.3"
+require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.7"
 
 @[default_target]
 lean_lib Tracer where
